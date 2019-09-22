43b060ffa05ff4b5e619cd6a97b584d449637048
Alec Morgan <Henrycalicomorgan@gmail.com>
2019-09-22 13:42:03 -0700

    Add reward visualizations and periodic print-outs for greater model interpretability

--- qlearning.py
+++ qlearning.py
@@ -1,5 +1,6 @@
 import gym
 import numpy as np
+import matplotlib.pyplot as plt
 
 env = gym.make("MountainCar-v0")
 env.reset()
@@ -10,9 +11,9 @@ LEARNING_RATE = 0.1
 # How valuable are future rewards? 
 DISCOUNT = 0.95
 # Training epochs.
-EPISODES = 5000
+EPISODES = 2000
 # How many episodes to wait in between rendering a given episode. 
-SHOW_EVERY = 1000
+SHOW_EVERY = 500
 
 """
 DISCRETE_OS_SIZE = discrete observation space size.
@@ -39,12 +40,17 @@ epsilon_decay_value = epsilon / (END_EPSILON_DECAYING - START_EPISLON_DECAYING)
 # Initialize Q-table with random values. 
 q_table = np.random.uniform(low=-2, high=0, size=(DISCRETE_OS_SIZE + [env.action_space.n]))
 
+ep_rewards = []
+aggr_ep_rewards = {'ep': [], 'avg': [], 'min': [], 'max': []}
+
 
 def get_discrete_state(state):
     discrete_state = (state - env.observation_space.low) / discrete_os_win_size
     return tuple(discrete_state.astype(np.int))
 
+
 for episode in range(EPISODES):
+    episode_reward = 0
     if episode % SHOW_EVERY == 0:
         render = True
     else:
@@ -59,8 +65,8 @@ for episode in range(EPISODES):
             action = np.random.randint(0, env.action_space.n)
 
         # Three actions: 0 = move left, 2 = move right, 1 = do nothing. 
-        action = np.argmax(q_table[discrete_state])
         new_state, reward, done, _ = env.step(action)
+        episode_reward += reward
         new_discrete_state = get_discrete_state(new_state)
 
         if render:
@@ -72,13 +78,13 @@ for episode in range(EPISODES):
             max_future_q = np.max(q_table[new_discrete_state])
             current_q = q_table[discrete_state + (action, )]
 
-            # New Q-value to update Q-table with and backpropagate through neural network. 
+            # New Q-value to update Q-table with.
             new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
             q_table[discrete_state + (action, )] = new_q
-            
+
         # If we reach our goal, "reward" = 0 instead of -1.
         elif new_state[0] >= env.goal_position:
-            print(f"We made it on episode {episode}")
+            # print(f"We made it on episode {episode}")
             q_table[discrete_state + (action, )] = 0
 
         discrete_state = new_discrete_state
@@ -86,5 +92,30 @@ for episode in range(EPISODES):
     # Over time epsilon will decrease, meaning that we will exploit more than we will explore
     if END_EPSILON_DECAYING >= episode >= START_EPISLON_DECAYING:
         epsilon -= epsilon_decay_value
+        
+    ep_rewards.append(episode_reward)
+    
+    if episode % 10 == 0:
+        np.save(f"qtables/qtable-{episode}.npy", q_table)
+    if episode % SHOW_EVERY == 0:
+        last_k_ep_rewards = ep_rewards[-SHOW_EVERY:]
+        average_reward = sum(last_k_ep_rewards) / len(last_k_ep_rewards)
+        aggr_ep_rewards['ep'].append(episode)
+        aggr_ep_rewards['avg'].append(average_reward)
+        aggr_ep_rewards['min'].append(min(last_k_ep_rewards))
+        aggr_ep_rewards['max'].append(max(last_k_ep_rewards))
+        
+        print(f"""
+        Episode: {episode} 
+        Avg: {average_reward} 
+        Min: {min(last_k_ep_rewards)} 
+        Max: {max(last_k_ep_rewards)}
+        """)
 
 env.close()
+
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['avg'], label="avg")
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['min'], label="min")
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['max'], label="max")
+plt.legend(loc=4)
+plt.show()
